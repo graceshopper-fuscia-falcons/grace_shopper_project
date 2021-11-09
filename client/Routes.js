2789b56b67a20311f94e32d82846da02c988baa8
Francesco <francesco.seddo@gmail.com>
2021-11-09 13:32:34 -0500

    added very basic AllFlowers component

--- client/Routes.js
+++ client/Routes.js
@@ -2,6 +2,7 @@ import React, {Component, Fragment} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
 import { Login, Signup } from './components/AuthForm';
+import { AllFlowers } from './components/AllFlowers';
 import Home from './components/Home';
 import {me} from './store'
 
@@ -28,6 +29,7 @@ class Routes extends Component {
             <Route path='/' exact component={ Login } />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
+            <Route path = "/flowers" component={AllFlowers} />
           </Switch>
         )}
       </div>
