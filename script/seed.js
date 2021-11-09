d3d263a442d6e61decf6dbf974fbbb3d80959d2b
Francesco <francesco.seddo@gmail.com>
2021-11-09 13:16:55 -0500

    refactored seed file

--- script/seed.js
+++ script/seed.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const {db, models: {User} } = require('../server/db')
+const {db, models: {User, Plant} } = require('../server/db')
 
 /**
  * seed - this function clears the database, updates tables to
@@ -9,6 +9,30 @@ const {db, models: {User} } = require('../server/db')
 async function seed() {
   await db.sync({ force: true }) // clears db and matches models to tables
   console.log('db synced!')
+  //Creating Plants
+  const plants = await Promise.all([
+  Plant.create({
+    name: 'Blooming Love™',
+    flowerColor: 'red',
+    flowerType: 'Rose',
+    imageURL: 'https://cdn3.1800flowers.com/wcsstore/Flowers/images/catalog/161132lx.jpg?width=545&height=597&quality=1&auto=webp&optimize={medium}'
+    }),
+
+   Plant.create({
+    name: 'Ocean Breeze Orchids',
+    flowerColor: 'blue',
+    flowerType: 'Orchids',
+    imageURL: 'https://cdn1.1800flowers.com/wcsstore/Flowers/images/catalog/140953mmdsv2wc2x.jpg?width=545&height=597&quality=1&auto=webp&optimize={medium}'
+    }),
+
+   Plant.create({
+    name: 'Harvest Glow™',
+    flowerColor: 'yellow',
+    flowerType: 'Sunflowers',
+    imageURL: 'https://cdn3.1800flowers.com/wcsstore/Flowers/images/catalog/174303lx.jpg?width=545&height=597&quality=80&auto=webp&optimize={medium}'
+    })
+  ])
+
 
   // Creating Users
   const users = await Promise.all([
@@ -17,6 +41,7 @@ async function seed() {
   ])
 
   console.log(`seeded ${users.length} users`)
+  console.log(`seeded ${plants.length} plants`)
   console.log(`seeded successfully`)
   return {
     users: {
