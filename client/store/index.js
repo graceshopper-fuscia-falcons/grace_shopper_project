450829c8d73fd867b73776276c33823f01e3d23b
Sean Vasvari <vasvarisean@gmail.com>
2021-11-09 23:45:36 -0500

    Cleaned up code

--- client/store/index.js
+++ client/store/index.js
@@ -3,9 +3,8 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
-import plantsReducer from './plants'
 
-const reducer = combineReducers({ auth, plantsReducer })
+const reducer = combineReducers({ auth })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
