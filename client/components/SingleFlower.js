04aeafe8fc6b65277a4be96addc74e9aa5938d6c
Sergio Rodriguez <SergioR133@icloud.com>
2021-11-12 12:15:45 -0500

    Added cart localStorage

--- client/components/SingleFlower.js
+++ client/components/SingleFlower.js
@@ -1,20 +1,35 @@
 import React from 'react'
 import axios from "axios"
-import {connect} from "react-redux"
+import { connect } from "react-redux"
 import { fetchPlant } from '../store/singlePlant'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import ls from 'local-storage'
 
 export class SingleFlower extends React.Component {
     constructor(){
         super()
+        if (!ls.get('cart')) {
+            ls.set('cart', [])
+        }
+        this.handleAddToCart = this.handleAddToCart.bind(this)
         this.state = {
-
         }
     }
 
     componentDidMount() {
         const plantId = this.props.match.params.flowersId
         const {data} = this.props.fetchPlant(plantId);
+    }
+
+    async handleAddToCart(){
+        let getCart = ls.get('cart');
+        let itemsToAdd = {
+            'name': this.props.targetFlower.name,
+            'price': this.props.targetFlower.price
+        }
+        let fillCart = getCart.push(JSON.stringify(itemsToAdd))
+        const myCart = ls.set("cart", getCart)
+        
     }
 
     render(){
@@ -25,7 +40,9 @@ export class SingleFlower extends React.Component {
                 <div>{targetFlower.flowerType}</div>
                 <div>{targetFlower.flowerColor}</div>
                 <img src ={targetFlower.imageUrl}></img>
-                <button>Add To Cart!</button>
+                <button 
+                    type="button"
+                    onClick={() => this.handleAddToCart()}>Add To Cart!</button>
                 <Link to={`/flowers`}>All Flowers</Link>
             </div>
         )
