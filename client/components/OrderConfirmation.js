c691459052401f9083d689c5d864f3b6fe887cb1
Sean Vasvari <vasvarisean@gmail.com>
2021-11-15 07:23:49 -0500

    CSS Update

--- client/components/OrderConfirmation.js
+++ client/components/OrderConfirmation.js
@@ -4,9 +4,9 @@ export default class OrderConfirmation extends React.Component {
     render() {
         return (
             <main>
-                <div className='CartContainer'>
-                    <div className="CartTitleContainer">
-                        <div className='CartTitle'>
+                <div className='OCContainer'>
+                    <div className="OCTitleContainer">
+                        <div className='OCTitle'>
                             <h1>Your order has been placed Successfully!</h1>
                         </div>
                     </div>
