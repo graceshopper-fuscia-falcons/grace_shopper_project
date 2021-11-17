c2f3f7e85a4a8ca3ea2617c1bb9bb386b9ad37f1
Sean Vasvari <vasvarisean@gmail.com>
2021-11-17 15:32:24 -0500

    fix

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import Welcome from './Welcome'
+import { connect } from 'react-redux'
 
 /**
  * COMPONENT
  */
 export const Home = props => {
-  const {username} = props
+  const { username } = props
 
   return (
-    <div>
-      <Welcome username={username} />
-    </div>
+    <main>
+      <div className='WelcomeContainer'>
+        <div className="WelcomeTitleContainer">
+          <div className='WelcomeTitle'>
+            <h1>Welcome {username}!</h1>
+          </div>
+          <button>Edit My Account</button>
+          <button>View Previous Orders</button>
+        </div>
+      </div>
+    </main>
   )
 }
 
