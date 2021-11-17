2e40ec4063a5525e4b7adbe99ad0f4ef9e0ca95a
Sean Vasvari <vasvarisean@gmail.com>
2021-11-17 13:36:18 -0500

    qf

--- client/store/singlePlant.js
+++ client/store/singlePlant.js
@@ -2,9 +2,11 @@ import Axios from 'axios';
 
 ////// Action Types
 const SET_PLANT = 'SET_PLANT'
+const CLEAR = 'CLEAR'
 
 ////// Action Creators
 export const setPlant = (plant) => ({type: SET_PLANT, plant})
+export const _clearPlant = () => ({type: CLEAR})
 
 ////// Async Creators
 export const fetchPlant = (plantId) => {
@@ -14,11 +16,19 @@ export const fetchPlant = (plantId) => {
     }
 }
 
+export const clearPlant = () => {
+  return async (dispatch) => {
+    dispatch(_clearPlant())
+  }
+}
+
 //////Reducer
 export default function (state = {}, action) {
     switch(action.type) {
       case SET_PLANT :
         return action.plant;
+      case CLEAR:
+        return {}
       default :
         return state;
     }
