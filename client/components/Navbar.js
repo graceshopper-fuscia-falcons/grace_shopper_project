bb328a8fdf8a368e559db1a213a7e9ecdae61186
Francesco <francesco.seddo@gmail.com>
2021-11-17 10:51:50 -0500

    updating navbar code

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -11,60 +11,10 @@ export class Navbar extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this)
-    this.state = {
-      userId: 0,
-      userType: '',
-      qty: this.props.userId ? this.props.cart.qty : (ls.get('cart') ? ls.get('cart').qty : 0)
-    }
-  }
-
-  async componentDidMount() {
-    const currentUser = await this.props.fetchMe();
-    const userType = currentUser ? 'member' : 'guest';
-    let qty = 0
-    let userId = 0
-    if (userType === 'guest') {
-      // await this.props.fetchLocalCart();
-      qty = ls.get('cart').qty
-    } else if (userType === 'member') {
-      await this.props.fetchCart(this.props.userId);
-      qty = this.props.cart.qty
-      userId = this.props.userId
-    }
-
-    this.setState({
-      userId,
-      userType,
-      qty
-    });
-  }
-
-  async componentDidUpdate() {
-    
-    const userType = this.props.userId ? 'member' : 'guest';
-    let qty = 0
-    if (userType === 'guest') {
-      if (this.state.qty !== ls.get('cart').qty) {
-        qty = ls.get('cart').qty
-        this.setState({
-          userType,
-          qty
-        });
-      }
-    } else if (userType === 'member') {
-      if (this.state.qty !== this.props.cart.qty) {
-        qty = this.props.cart.qty
-        this.setState({
-          userType,
-          qty
-        });
-      }
-    }
   }
 
   async handleClick() {
     this.props.logout()
-    await this.props.fetchLocalCart()
   }
 
   render() {
